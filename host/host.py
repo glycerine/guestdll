b1241c42b4190749e44c2062eee0dfbac32873d1
J <j.e.aten@gmail.com>
2019-04-28 17:50:59 +0100

    latest progress

--- host/host.py
+++ host/host.py
@@ -1,16 +1,21 @@
+#!/usr/bin/python
 from ctypes import *
 import _ctypes
 
 runtimefn = '/usr/local/go/pkg/linux_amd64_dynlink/libruntime,sync-atomic.so'
 rt = CDLL(runtimefn)
 
-rtinit = getattr(rt, "runtime.init")
-rtinit()
-rtmain = getattr(rt, "runtime.main")
-rtmain()
+## go executables appear to initialize by calling _rt0_amd64_linux
+rt0 = getattr(rt,"_rt0_amd64_linux")
+rt0()
 
-guestfn = '/root/vol/go/pkg/linux_amd64_dynlink/libgithub.com-glycerine-guestdll-guest.so'
-g = CDLL(guestfn)
-g.guest.restype = c_int
-print g.guest()
-_ctypes.dlclose(g._handle)
+#rtinit = getattr(rt, "runtime.init")
+#rtinit()
+#rtmain = getattr(rt, "runtime.main")
+#rtmain()
+
+#guestfn = '/root/vol/go/pkg/linux_amd64_dynlink/libgithub.com-glycerine-guestdll-guest.so'
+#g = CDLL(guestfn)
+#g.guest.restype = c_int
+#print g.guest()
+#_ctypes.dlclose(g._handle)
