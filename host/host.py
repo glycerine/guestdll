1184f2ec3e20a53ef8ef0035eb02f9a42801dd4b
J <j.e.aten@gmail.com>
2019-04-28 17:21:51 +0100

    latest host, crashes calling runtime.init()

--- host/host.py
+++ host/host.py
@@ -1,10 +1,16 @@
 from ctypes import *
 import _ctypes
-fn = '/root/vol/go/pkg/linux_amd64_dynlink/libgithub.com-glycerine-guestdll-guest.so'
-runtime = '/usr/local/go/pkg/linux_amd64_dynlink/libruntime,sync-atomic.so'
-CDLL(runtime)
-## OSError: /usr/local/go/pkg/linux_amd64_dynlink/libruntime,sync-atomic.so: undefined symbol: main.init
-##lib = CDLL(fn)
-#lib.guest.restype = c_int
-#print lib.guest()
-#_ctypes.dlclose(lib._handle)
+
+runtimefn = '/usr/local/go/pkg/linux_amd64_dynlink/libruntime,sync-atomic.so'
+rt = CDLL(runtimefn)
+
+rtinit = getattr(rt, "runtime.init")
+rtinit()
+rtmain = getattr(rt, "runtime.main")
+rtmain()
+
+guestfn = '/root/vol/go/pkg/linux_amd64_dynlink/libgithub.com-glycerine-guestdll-guest.so'
+g = CDLL(guestfn)
+g.guest.restype = c_int
+print g.guest()
+_ctypes.dlclose(g._handle)
