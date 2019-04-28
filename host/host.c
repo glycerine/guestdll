88fc5ec4f87559af4f757e447d902914bb8fe560
Jason E. Aten <j.e.aten@gmail.com>
2019-04-28 22:31:16 +0000

    host.c replaces host.py

---
+++ host/host.c
@@ -0,0 +1,30 @@
+#include "stdlib.h"
+#include "stdio.h"
+#include "dlfcn.h"
+
+// NB change this to reflect the path to your runtime library.
+const char* runtimeFn = "/usr/local/go/pkg/linux_amd64_dynlink/libruntime,sync-atomic.so";
+
+int main() {
+  void *rtHandle;
+  void (*runtimeStart)();
+  char *error;
+  
+  printf("host.c main has begun.\n");
+  rtHandle = dlopen(runtimeFn, RTLD_LAZY | RTLD_GLOBAL | RTLD_NODELETE);
+  if (!rtHandle) {
+    fprintf(stderr, "%s\n", dlerror());
+    exit(EXIT_FAILURE);
+  }
+  dlerror();    /* Clear any existing error */  
+  runtimeStart = dlsym(rtHandle, "_rt0_amd64_linux");
+
+  error = dlerror();
+  if (error != NULL) {
+    fprintf(stderr, "%s\n", error);
+    exit(EXIT_FAILURE);
+  }
+  printf("good: about to invoke runtimeStart().\n");    
+  runtimeStart();
+  printf("host.c main is done.\n");  
+}
